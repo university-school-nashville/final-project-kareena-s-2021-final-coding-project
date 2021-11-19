4f0a6d5fd330a92190aa716c8f33cd4ab53789e0
kareenakloek <94002050+kareenakloek@users.noreply.github.com>
2021-11-19 09:21:19 -0600

    Update kareena_final_project.py
    I added more to my code, before it just said "this is my py file", now it has some basic coding from my actually python file.

--- kareena_final_project.py
+++ kareena_final_project.py
@@ -5,5 +5,15 @@ Created on Thu Nov 11 08:58:55 2021
 
 @author: kareenakloek
 """
+print("let's play a game of tic tac toe")
+print("You are x's and I am o's")
 
-"""this is my py file for my final project"""
+x = int(input("pick a number from 1 - 9 to make a move"))
+print(x)
+
+print("1 / 2 / 3")
+print("- + - + - ")
+print("4 / 5 / 6")
+print("- + - + - ")
+print("7 / 8 / 9")
+
